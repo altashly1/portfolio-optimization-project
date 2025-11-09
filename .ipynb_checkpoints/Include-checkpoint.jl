93836413c0b81b786960f2443ac1ee54eb81f731
Abdulrahman Alswaidan <aa2725@cornell.edu>
2025-11-09 16:03:56 -0500

    updates

--- .ipynb_checkpoints/Include-checkpoint.jl
+++ .ipynb_checkpoints/Include-checkpoint.jl
@@ -2,11 +2,12 @@
 const _ROOT = pwd();
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
+const _PATH_TO_FIGS = joinpath(_ROOT, "figs");
+
 
 # make sure all is up to date -
 using Pkg
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
-    Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
     Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 end
 
--- Include.jl
+++ Include.jl
@@ -8,7 +8,6 @@ const _PATH_TO_FIGS = joinpath(_ROOT, "figs");
 # make sure all is up to date -
 using Pkg
 if (isfile(joinpath(_ROOT, "Manifest.toml")) == false) # have manifest file, we are good. Otherwise, we need to instantiate the environment
-    Pkg.add(path="https://github.com/varnerlab/VLQuantitativeFinancePackage.jl.git")
     Pkg.activate("."); Pkg.resolve(); Pkg.instantiate(); Pkg.update();
 end
 
