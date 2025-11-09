d6ee29666a5e5ac1d7cf1cd32b7ed84e73b50448
Abdulrahman Alswaidan <aa2725@cornell.edu>
2025-11-09 14:39:33 -0500

    updates

--- .ipynb_checkpoints/Include-checkpoint.jl
+++ .ipynb_checkpoints/Include-checkpoint.jl
@@ -2,8 +2,6 @@
 const _ROOT = pwd();
 const _PATH_TO_SRC = joinpath(_ROOT, "src");
 const _PATH_TO_DATA = joinpath(_ROOT, "data");
-const _PATH_TO_FIGS = joinpath(_ROOT, "figs");
-
 
 # make sure all is up to date -
 using Pkg
